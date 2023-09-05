77209efde35d37f16f146f21e82c5a67d3dc189a
Pedro Augusto Pereira <pereiraaugustopedro2021@gmail.com>
2023-09-04 23:12:01 -0300

    adicionando função de remover like

--- src/controllers/favoritesController.ts
+++ src/controllers/favoritesController.ts
@@ -41,10 +41,10 @@ export const favoritesController = {
     //DELETE /favorite/:id
     delete: async (req: AuthenticatedRequest, res: Response) => {
         const userId = req.user!.id;
-        const { courseId } = req.body;
+        const courseId = req.params.id;
 
         try {
-            await favoriteService.delete(userId, courseId);
+            await favoriteService.delete(userId, Number(courseId));
             res.status(204).send();
         } catch (err) {
             if (err instanceof Error) {
